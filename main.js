792fc36be1775b85410c9cd603dd05155d1d372b
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-08-07 14:22:47 +0530

    safdsa

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const fs = require('fs');
 
 // TODO: Write the message "Hello, World!" to the file "output.txt"
+const express=require('express')
+const app=express();
+fs.writeFile('./output.txt',"Hello, World!",(err)=>{
+    console.log("any err if ",err);
 
+})
+
+app.listen(3000,()=>{
+    console.log("we are linstening to port 3000")
+})
